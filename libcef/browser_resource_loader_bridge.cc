42846e6e31e184a88da116b456254b0d46853766
Marshall Greenblatt <magreenblatt@gmail.com>
2011-02-16 03:19:07 +0000

    Fix Mac compile error.
    git-svn-id: https://chromiumembedded.googlecode.com/svn/trunk@187 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- libcef/browser_resource_loader_bridge.cc
+++ libcef/browser_resource_loader_bridge.cc
@@ -202,11 +202,11 @@ class RequestProxy : public net::URLRequest::Delegate,
 
         if (allow_download &&
             webkit_glue::ShouldDownload(content_disposition, info.mime_type)) {
-          FilePath path(net::GetSuggestedFilename(url, content_disposition,
-              info.charset, ASCIIToUTF16("download")));
+          string16 filename = net::GetSuggestedFilename(url,
+              content_disposition, info.charset, ASCIIToUTF16("download"));
           CefRefPtr<CefDownloadHandler> dl_handler;
           if (handler->HandleDownloadResponse(browser_, info.mime_type,
-                  path.value(), info.content_length, dl_handler) ==
+                  filename, info.content_length, dl_handler) ==
                   RV_CONTINUE) {
             download_handler_ = dl_handler;
           }
