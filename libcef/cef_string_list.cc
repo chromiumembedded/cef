2c5534d3083bc93375f95dfb63986c167411cfd9
Marshall Greenblatt <magreenblatt@gmail.com>
2011-05-21 01:49:16 +0000

    Fix compile errors.
    git-svn-id: https://chromiumembedded.googlecode.com/svn/trunk@239 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- libcef/cef_string_list.cc
+++ libcef/cef_string_list.cc
@@ -2,7 +2,7 @@
 // reserved. Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
 
-#include "include/cef_string_list.h"
+#include "include/internal/cef_string_list.h"
 #include "base/logging.h"
 #include <vector>
 
--- libcef/cef_string_map.cc
+++ libcef/cef_string_map.cc
@@ -2,7 +2,7 @@
 // reserved. Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
 
-#include "include/cef_string_map.h"
+#include "include/internal/cef_string_map.h"
 #include "base/logging.h"
 #include <map>
 
