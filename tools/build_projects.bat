9fef43d165a1f16bffa30fd94a92c52eccf754f2
Marshall Greenblatt <magreenblatt@gmail.com>
2011-10-11 18:19:13 +0000

    Add the build_projects.bat script and remove the pause from make_cppdocs.bat (issue #373).
    git-svn-id: https://chromiumembedded.googlecode.com/svn/trunk@309 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

---
+++ tools/build_projects.bat
@@ -0,0 +1,26 @@
+@echo off
+
+if "%1" == "" (
+echo ERROR: Please specify a build target: Debug or Release
+goto end
+)
+
+if "%2" == "" (
+set PROJECT_NAME=cefclient
+) else (
+set PROJECT_NAME=%2
+)
+
+echo Configuring Visual Studio environment...
+if "%GYP_MSVS_VERSION%" == "2008" (
+call "%VS90COMNTOOLS%vsvars32.bat"
+set PROJECT_EXT=.vcproj
+) else (
+call "%VS100COMNTOOLS%vsvars32.bat"
+set PROJECT_EXT=.vcxproj
+)
+
+echo Building %1 target for %PROJECT_NAME% project...
+devenv.com /build %1 ..\cef.sln /project %PROJECT_NAME%%PROJECT_EXT%
+
+:end
