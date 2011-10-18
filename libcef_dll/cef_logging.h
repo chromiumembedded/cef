719886c23280b1f8c5fa49222e2876ca92aacbef
Marshall Greenblatt <magreenblatt@gmail.com>
2011-10-18 16:51:34 +0000

    Add #ifndef guards to cef_logging.h
    git-svn-id: https://chromiumembedded.googlecode.com/svn/trunk@322 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- libcef_dll/cef_logging.h
+++ libcef_dll/cef_logging.h
@@ -2,9 +2,14 @@
 // reserved. Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
 
+#ifndef _CEF_LOGGING_H
+#define _CEF_LOGGING_H
+
 #ifdef BUILDING_CEF_SHARED
 #include "base/logging.h"
 #else
 #include <assert.h>
 #define DCHECK(condition) assert(condition)
 #endif
+
+#endif // _CEF_LOGGING_H
