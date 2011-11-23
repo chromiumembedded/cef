559caafd1db1b12cafa0cda6963b3a278956b622
Marshall Greenblatt <magreenblatt@gmail.com>
2011-11-23 23:24:01 +0000

    Revert revision 395 changes to print_settings.cc.
    git-svn-id: https://chromiumembedded.googlecode.com/svn/trunk@396 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- libcef/printing/print_settings.cc
+++ libcef/printing/print_settings.cc
@@ -11,7 +11,7 @@
 namespace printing {
 
 // Global SequenceNumber used for generating unique cookie values.
-static base::AtomicSequenceNumber cookie_seq = LAZY_INSTANCE_INITIALIZER;
+static base::AtomicSequenceNumber cookie_seq(base::LINKER_INITIALIZED);
 
 PageMeasurements::PageMeasurements()
     : page_type(PT_LETTER),
