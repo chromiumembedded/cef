8d1abe7d70e5d16d1ebaef0b77a9b4df6bc2962a
Marshall Greenblatt <magreenblatt@gmail.com>
2011-12-31 01:01:42 +0000

    Linux: Fix cefclient shutdown crash.
    git-svn-id: https://chromiumembedded.googlecode.com/svn/trunk@450 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- tests/cefclient/cefclient_gtk.cpp
+++ tests/cefclient/cefclient_gtk.cpp
@@ -25,8 +25,7 @@ char szWorkingDir[512]; // The current working directory
 extern CefRefPtr<ClientHandler> g_handler;
 
 void destroy(void) {
-  CefShutdown();
-  exit(0);
+  CefQuitMessageLoop();
 }
 
 void TerminationSignalHandler(int signatl) {
@@ -425,6 +424,8 @@ int main(int argc, char *argv[]) {
   signal(SIGTERM, TerminationSignalHandler);
 
   CefRunMessageLoop();
+
+  CefShutdown();
 
   return 0;
 }
