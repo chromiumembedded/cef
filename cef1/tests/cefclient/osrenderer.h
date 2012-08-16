a2ca607a7751fda0aa9bce15e24ff87302a72779
Marshall Greenblatt <magreenblatt@gmail.com>
2012-08-16 19:21:47 +0000

    Optimize the cefclient off-screen rendering example (issue #697).
    git-svn-id: https://chromiumembedded.googlecode.com/svn/trunk@743 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- cef1/tests/cefclient/osrenderer.h
+++ cef1/tests/cefclient/osrenderer.h
@@ -20,9 +20,6 @@ class ClientOSRenderer {
   // Clean up the OpenGL environment.
   void Cleanup();
 
-  // Set the size of the viewport.
-  void SetSize(int width, int height);
-
   // Render to the screen.
   void Render();
 
@@ -40,29 +37,18 @@ class ClientOSRenderer {
   void SetSpin(float spinX, float spinY);
   void IncrementSpin(float spinDX, float spinDY);
 
-  // Retrieve the pixel value from the view buffer. |x| and |y| are relative to
-  // the upper-left corner of the view.
-  bool GetPixelValue(int x, int y, unsigned char& r, unsigned char& g,
-                     unsigned char& b, unsigned char& a);
-
   bool IsTransparent() { return transparent_; }
 
+  int GetViewWidth() { return view_width_; }
+  int GetViewHeight() { return view_height_; }
+
  private:
-  void SetBufferSize(int width, int height, bool view);
-  void SetRGBA(const void* src, int width, int height, bool view);
-  void SetRGB(const void* src, int width, int height, bool view);
-  void DrawViewBuffer(int max_width, int max_height);
- 
   bool transparent_;
   bool initialized_;
   unsigned int texture_id_;
-  unsigned char* view_buffer_;
-  int view_buffer_size_;
-  unsigned char* popup_buffer_;
-  int popup_buffer_size_;
-  CefRect popup_rect_;
   int view_width_;
   int view_height_;
+  CefRect popup_rect_;
   float spin_x_;
   float spin_y_;
 };
