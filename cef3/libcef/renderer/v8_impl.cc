358faf993a2ab9df4619c9b98942548c3ff632b1
Marshall Greenblatt <magreenblatt@gmail.com>
2012-11-05 22:33:26 +0000

    Fix potential crash if a context with no bindings is released (issue #736).
    git-svn-id: https://chromiumembedded.googlecode.com/svn/branches/1271@901 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- cef3/libcef/renderer/v8_impl.cc
+++ cef3/libcef/renderer/v8_impl.cc
@@ -88,6 +88,9 @@ class CefV8TrackManager {
 
       v8::Handle<v8::Object> object = context->Global();
       v8::Handle<v8::Value> value = object->GetHiddenValue(context_state_key_);
+      if (value.IsEmpty())
+        return;
+
       if (!value.IsEmpty())
         return static_cast<CefV8ContextState*>(v8::External::Unwrap(value));
 
