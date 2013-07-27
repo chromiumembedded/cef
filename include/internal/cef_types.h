e874af0891d69b16ffe8a4737c8e38dda7913394
Marshall Greenblatt <magreenblatt@gmail.com>
2013-07-27 00:25:31 +0000

    Update CefSettings.cache_path documentation.
    git-svn-id: https://chromiumembedded.googlecode.com/svn/trunk@1346 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- include/internal/cef_types.h
+++ include/internal/cef_types.h
@@ -192,8 +192,9 @@ typedef struct _cef_settings_t {
 
   ///
   // The location where cache data will be stored on disk. If empty an in-memory
-  // cache will be used. HTML5 databases such as localStorage will only persist
-  // across sessions if a cache path is specified.
+  // cache will be used for some features and a temporary disk cache for others.
+  // HTML5 databases such as localStorage will only persist across sessions if a
+  // cache path is specified.
   ///
   cef_string_t cache_path;
 
