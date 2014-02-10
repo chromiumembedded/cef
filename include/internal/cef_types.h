a0be2475274ba3d76a1f0a4a11186823bd45c172
Marshall Greenblatt <magreenblatt@gmail.com>
2014-02-10 16:38:35 +0000

    Use int instead of bool for C API compatibility (issue #1196).
    git-svn-id: https://chromiumembedded.googlecode.com/svn/trunk@1605 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- include/internal/cef_types.h
+++ include/internal/cef_types.h
@@ -224,11 +224,11 @@ typedef struct _cef_settings_t {
   int multi_threaded_message_loop;
 
   ///
-  // Set to true to enable windowless (off-screen) rendering support. Do not
+  // Set to true (1) to enable windowless (off-screen) rendering support. Do not
   // enable this value if the application does not use windowless rendering as
   // it may reduce rendering performance on some systems.
   ///
-  bool windowless_rendering_enabled;
+  int windowless_rendering_enabled;
 
   ///
   // Set to true (1) to disable configuration of browser process features using
