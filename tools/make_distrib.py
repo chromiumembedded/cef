a1fcfe8dc5a6792b7a2c1adb9c655c579afc636a
Marshall Greenblatt <magreenblatt@gmail.com>
2014-08-07 14:41:41 +0000

    Add support for large zip files in make_distrib.py (issue #1350).
    git-svn-id: https://chromiumembedded.googlecode.com/svn/trunk@1795 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- tools/make_distrib.py
+++ tools/make_distrib.py
@@ -17,7 +17,7 @@ import zipfile
 
 def create_archive(input_dir, zip_file):
   """ Creates a zip archive of the specified input directory. """
-  zf = zipfile.ZipFile(zip_file, 'w', zipfile.ZIP_DEFLATED)
+  zf = zipfile.ZipFile(zip_file, 'w', zipfile.ZIP_DEFLATED, True)
   def addDir(dir):
     for f in os.listdir(dir):
       full_path = os.path.join(dir, f)
