2d12b430d3bef0e28344caf9f37c936071927313
Marshall Greenblatt <magreenblatt@gmail.com>
2014-08-14 09:42:23 +0000

    Linux: Fix type mismatch compile error (issue #1354).
    git-svn-id: https://chromiumembedded.googlecode.com/svn/trunk@1807 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- tests/cefclient/client_handler_gtk.cpp
+++ tests/cefclient/client_handler_gtk.cpp
@@ -243,7 +243,7 @@ void ClientHandler::OnTitleChange(CefRefPtr<CefBrowser> browser,
 
     // Retrieve the X11 window handle for the browser.
     ::Window window = browser->GetHost()->GetWindowHandle();
-    DCHECK_NE(window, kNullWindowHandle);
+    DCHECK(window != kNullWindowHandle);
 
     // Retrieve the atoms required by the below XChangeProperty call.
     const char* kAtoms[] = {
