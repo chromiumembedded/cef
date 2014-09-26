75097e04e8d64f373fcf7f51ee022607fecd7023
Marshall Greenblatt <magreenblatt@gmail.com>
2014-09-26 23:49:21 +0000

    Add documentation for cef_key_event_type_t values.
    git-svn-id: https://chromiumembedded.googlecode.com/svn/branches/2062@1847 5089003a-bbd8-11dd-ad1f-f1f9622dbc98

--- include/internal/cef_types.h
+++ include/internal/cef_types.h
@@ -1464,9 +1464,28 @@ typedef enum {
 // Key event types.
 ///
 typedef enum {
+  ///
+  // Notification that a key transitioned from "up" to "down".
+  ///
   KEYEVENT_RAWKEYDOWN = 0,
+
+  ///
+  // Notification that a key was pressed. This does not necessarily correspond
+  // to a character depending on the key and language. Use KEYEVENT_CHAR for
+  // character input.
+  ///
   KEYEVENT_KEYDOWN,
+
+  ///
+  // Notification that a key was released.
+  ///
   KEYEVENT_KEYUP,
+
+  ///
+  // Notification that a character was typed. Use this for text input. Key
+  // down events may generate 0, 1, or more than one character event depending
+  // on the key, locale, and operating system.
+  ///
   KEYEVENT_CHAR
 } cef_key_event_type_t;
 
