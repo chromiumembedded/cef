2d0c79ec29422fc1ab991490f5293d20f81118d1
Marshall Greenblatt <magreenblatt@gmail.com>
2015-08-17 18:44:17 -0400

    Add missing OVERRIDE specifier to avoid compile errors with clang 3.6 (issue #1688)

--- include/cef_runnable.h
+++ include/cef_runnable.h
@@ -148,7 +148,7 @@ class CefRunnableMethod : public CefTask {
       traits_.ReleaseCallee(obj);
   }
 
-  virtual void Execute() {
+  void Execute() OVERRIDE {
     if (obj_)
       DispatchToMethod(obj_, meth_, params_);
   }
@@ -240,7 +240,7 @@ class CefRunnableFunction : public CefTask {
   ~CefRunnableFunction() {
   }
 
-  virtual void Execute() {
+  void Execute() OVERRIDE {
     if (function_)
       DispatchToFunction(function_, params_);
   }
