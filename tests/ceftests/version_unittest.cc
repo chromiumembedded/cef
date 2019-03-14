2a40650926ae89844691d6dbee3c950c14fc57f1
Marshall Greenblatt <magreenblatt@gmail.com>
2019-03-14 17:37:47 -0400

    Compute all version numbers using cef_version.py (see issue #2596)
    The version format can now be controlled by setting the CEF_OLD_VERSION_FORMAT
    environment variable. The old format is currently the default.

--- tests/ceftests/version_unittest.cc
+++ tests/ceftests/version_unittest.cc
@@ -8,11 +8,13 @@
 
 TEST(VersionTest, VersionInfo) {
   EXPECT_EQ(CEF_VERSION_MAJOR, cef_version_info(0));
-  EXPECT_EQ(CEF_COMMIT_NUMBER, cef_version_info(1));
-  EXPECT_EQ(CHROME_VERSION_MAJOR, cef_version_info(2));
-  EXPECT_EQ(CHROME_VERSION_MINOR, cef_version_info(3));
-  EXPECT_EQ(CHROME_VERSION_BUILD, cef_version_info(4));
-  EXPECT_EQ(CHROME_VERSION_PATCH, cef_version_info(5));
+  EXPECT_EQ(CEF_VERSION_MINOR, cef_version_info(1));
+  EXPECT_EQ(CEF_VERSION_PATCH, cef_version_info(2));
+  EXPECT_EQ(CEF_COMMIT_NUMBER, cef_version_info(3));
+  EXPECT_EQ(CHROME_VERSION_MAJOR, cef_version_info(4));
+  EXPECT_EQ(CHROME_VERSION_MINOR, cef_version_info(5));
+  EXPECT_EQ(CHROME_VERSION_BUILD, cef_version_info(6));
+  EXPECT_EQ(CHROME_VERSION_PATCH, cef_version_info(7));
 }
 
 TEST(VersionTest, ApiHash) {
