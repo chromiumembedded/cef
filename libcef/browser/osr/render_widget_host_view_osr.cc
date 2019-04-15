5fa3d519c956772f8101b3abde8a94d00a3dce6d
Riku Palomäki <riku.palomaki@multitaction.com>
2019-04-15 17:25:30 +0000

    Fix assert failure when hiding the OSR widget (fixes issue #2631)

--- libcef/browser/osr/render_widget_host_view_osr.cc
+++ libcef/browser/osr/render_widget_host_view_osr.cc
@@ -535,6 +535,8 @@ void CefRenderWidgetHostViewOSR::Hide() {
   if (!is_showing_)
     return;
 
+  is_showing_ = false;
+
   if (browser_impl_.get())
     browser_impl_->CancelContextMenu();
 
@@ -547,8 +549,6 @@ void CefRenderWidgetHostViewOSR::Hide() {
   GetDelegatedFrameHost()->WasHidden();
   GetDelegatedFrameHost()->DetachFromCompositor();
 #endif
-
-  is_showing_ = false;
 }
 
 bool CefRenderWidgetHostViewOSR::IsShowing() {
