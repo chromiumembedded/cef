4907ec52f3fe52eea2c5b759f6bbf7c99a295afe
Marshall Greenblatt <magreenblatt@gmail.com>
2019-10-29 17:29:29 -0400

    Windows: Fix cef_sandbox.lib link errors (fixes issue #2792)

--- tools/make_distrib.py
+++ tools/make_distrib.py
@@ -845,6 +845,7 @@ if platform == 'windows':
   sandbox_libs = [
       'obj\\base\\base.lib',
       'obj\\base\\base_static.lib',
+      'obj\\base\\third_party\\double_conversion\\double_conversion.lib',
       'obj\\base\\third_party\\dynamic_annotations\\dynamic_annotations.lib',
       'obj\\base\\win\\pe_image.lib',
       cef_sandbox_lib,
