ac2a9cd693458774291ab4c973d3b383d4f1dc31
Marshall Greenblatt <magreenblatt@gmail.com>
2019-11-06 15:45:50 -0500

    Update API hashes for changes in commit 65f97a6

--- include/cef_api_hash.h
+++ include/cef_api_hash.h
@@ -34,7 +34,7 @@
 // implementations. See the translator.README.txt file in the tools directory
 // for more information.
 //
-// $hash=714707190282ccff4cfc40f0f17b88c24f27236a$
+// $hash=2e1f9d8f965b343a850bf5464700de36913f661e$
 //
 
 #ifndef CEF_INCLUDE_API_HASH_H_
@@ -47,13 +47,13 @@
 // way that may cause binary incompatibility with other builds. The universal
 // hash value will change if any platform is affected whereas the platform hash
 // values will change only if that particular platform is affected.
-#define CEF_API_HASH_UNIVERSAL "2b148bd47ce42d97377baaa92039bf1bb04d631e"
+#define CEF_API_HASH_UNIVERSAL "3ad9591f7024619a1e2fd796c7ad51ae3a34abbb"
 #if defined(OS_WIN)
-#define CEF_API_HASH_PLATFORM "fdc417626039e5cb7122cd531fcc82d3845354f4"
+#define CEF_API_HASH_PLATFORM "3b8e38ae40c1a930227c6277c39dbc24081ccec1"
 #elif defined(OS_MACOSX)
-#define CEF_API_HASH_PLATFORM "184c18fff7d61c248062eb43092c8f6f94f051b0"
+#define CEF_API_HASH_PLATFORM "4a77f1908fabccdb01a2d3bdbede5ca09fbaa394"
 #elif defined(OS_LINUX)
-#define CEF_API_HASH_PLATFORM "acc3cea452fa30b32d76d09282744a5dc30b9530"
+#define CEF_API_HASH_PLATFORM "434da6be7db70fd90c4265118ac47ace4073c6ae"
 #endif
 
 #ifdef __cplusplus
