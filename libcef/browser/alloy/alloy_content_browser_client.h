4adc23a52a527d71e26da35048c0261c8a073bc0
Marshall Greenblatt <magreenblatt@gmail.com>
2022-02-02 14:45:53 -0500

    alloy: Fix extension function registration for print preview dialog (fixes issue #3167)

--- libcef/browser/alloy/alloy_content_browser_client.h
+++ libcef/browser/alloy/alloy_content_browser_client.h
@@ -237,6 +237,7 @@ class AlloyContentBrowserClient : public content::ContentBrowserClient {
   bool ShouldAllowPluginCreation(
       const url::Origin& embedder_origin,
       const content::PepperPluginInfo& plugin_info) override;
+  void OnWebContentsCreated(content::WebContents* web_contents) override;
   bool IsFindInPageDisabledForOrigin(const url::Origin& origin) override;
 
   CefRefPtr<CefRequestContextImpl> request_context() const;
