5c56e98fc64d0d20f80634bcef290b641dadb392
Marshall Greenblatt <marshall@spotify.com>
2024-06-14 15:12:08 +0000

    Update to Chromium version 126.0.6478.62

--- include/cef_api_hash.h
+++ include/cef_api_hash.h
@@ -42,13 +42,13 @@
 // way that may cause binary incompatibility with other builds. The universal
 // hash value will change if any platform is affected whereas the platform hash
 // values will change only if that particular platform is affected.
-#define CEF_API_HASH_UNIVERSAL "3ecad1c27a9f10720824f4c4be478dab28f1258d"
+#define CEF_API_HASH_UNIVERSAL "ed1dfa5ff8a041241f8fb72eb7454811f358f0d3"
 #if defined(OS_WIN)
-#define CEF_API_HASH_PLATFORM "551fbcb8ec2687c554be82cc10f471201c88727b"
+#define CEF_API_HASH_PLATFORM "0d99d1b9b85b2efab91a39d6fc325bb6d56fd524"
 #elif defined(OS_MAC)
-#define CEF_API_HASH_PLATFORM "4ca22671083a799f3c8c09905804882988ebd97b"
+#define CEF_API_HASH_PLATFORM "e585e190387e31a71267207b66d175e213991470"
 #elif defined(OS_LINUX)
-#define CEF_API_HASH_PLATFORM "fb827bbeab0a16044f5cee11912d7dfc2ad44999"
+#define CEF_API_HASH_PLATFORM "09d3e280ed38f7a082b794c56ff71c52f86f0ea8"
 #endif
 
 #ifdef __cplusplus
