chromiumembedded/cef

commit 81a5005bc0ecfa692f579eb6ec53c9e9bd734b14

Add NUM_VALUES for enums and size for structs (see #3836)
API versioning requires that enumerations end with a count value
(`*_NUM_VALUES`) and structs begin with a size value (`size_t size`).
Wrapper templates are updated to support structs with different size
values indicating different versions.

To test:
Run `ceftests --gtest_filter=ApiVersionTest.StructVersion*`

libcef/browser/osr/video_consumer_osr.cc
@@ -137,7 +137,8 @@ void CefVideoConsumerOSR::OnFrameCaptured(
                             : CEF_COLOR_TYPE_BGRA_8888;
 
     // Build extra common info.
-    cef_accelerated_paint_info_common_t extra = {};
+    cef_accelerated_paint_info_common_t extra = {
+        sizeof(cef_accelerated_paint_info_common_t)};
     extra.timestamp = info->timestamp.InMicroseconds();
     extra.coded_size = {info->coded_size.width(), info->coded_size.height()};
     extra.visible_rect = {info->visible_rect.x(), info->visible_rect.y(),

cmake/cef_variables.cmake.in
@@ -186,6 +186,8 @@ if(OS_LINUX)
   if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
     list(APPEND CEF_CXX_COMPILER_FLAGS
       -Wno-attributes             # The cfi-icall attribute is not supported by the GNU C++ compiler
+      -Wno-array-bounds           # Silence "is partly outside array bounds" errors with runtime size check in wrapper
+      -Wno-stringop-overflow      # Silence "overflows the destination" errors with runtime size check in wrapper
       )
   endif()
 

include/internal/cef_linux.h
@@ -53,7 +53,7 @@ class CefMainArgs : public cef_main_args_t {
 struct CefWindowInfoTraits {
   typedef cef_window_info_t struct_type;
 
-  static inline void init(struct_type* s) {}
+  static inline void init(struct_type* s) { s->size = sizeof(struct_type); }
 
   static inline void clear(struct_type* s) {
     cef_string_clear(&s->window_name);

include/internal/cef_mac.h
@@ -53,7 +53,7 @@ class CefMainArgs : public cef_main_args_t {
 struct CefWindowInfoTraits {
   typedef cef_window_info_t struct_type;
 
-  static inline void init(struct_type* s) {}
+  static inline void init(struct_type* s) { s->size = sizeof(struct_type); }
 
   static inline void clear(struct_type* s) {
     cef_string_clear(&s->window_name);

include/internal/cef_types_color.h
@@ -48,6 +48,8 @@ typedef enum {
   /// BGRA with 8 bits per pixel (32bits total).
   ///
   CEF_COLOR_TYPE_BGRA_8888,
+
+  CEF_COLOR_TYPE_NUM_VALUES,
 } cef_color_type_t;
 
 #ifdef __cplusplus

include/internal/cef_types_content_settings.h
@@ -45,7 +45,7 @@ typedef enum {
   /// provided context. However, it may be overridden by other settings. This
   /// enum should NOT be read directly to determine whether cookies are enabled;
   /// the client should instead rely on the CookieSettings API.
-  CEF_CONTENT_SETTING_TYPE_COOKIES = 0,
+  CEF_CONTENT_SETTING_TYPE_COOKIES,
   CEF_CONTENT_SETTING_TYPE_IMAGES,
   CEF_CONTENT_SETTING_TYPE_JAVASCRIPT,
 
@@ -483,6 +483,8 @@ typedef enum {
   /// Checks whether cookies scope is handled according to origin-bound cookies
   /// or legacy behavior.
   CEF_CONTENT_SETTING_TYPE_LEGACY_COOKIE_SCOPE,
+
+  CEF_CONTENT_SETTING_TYPE_NUM_VALUES,
 } cef_content_setting_types_t;
 
 ///
@@ -490,14 +492,14 @@ typedef enum {
 /// ContentSetting type.
 ///
 typedef enum {
-  CEF_CONTENT_SETTING_VALUE_DEFAULT = 0,
+  CEF_CONTENT_SETTING_VALUE_DEFAULT,
   CEF_CONTENT_SETTING_VALUE_ALLOW,
   CEF_CONTENT_SETTING_VALUE_BLOCK,
   CEF_CONTENT_SETTING_VALUE_ASK,
   CEF_CONTENT_SETTING_VALUE_SESSION_ONLY,
   CEF_CONTENT_SETTING_VALUE_DETECT_IMPORTANT_CONTENT,
 
-  CEF_CONTENT_SETTING_VALUE_NUM_VALUES
+  CEF_CONTENT_SETTING_VALUE_NUM_VALUES,
 } cef_content_setting_values_t;
 
 #ifdef __cplusplus

include/internal/cef_types_linux.h
@@ -87,6 +87,11 @@ typedef struct _cef_main_args_t {
 ///
 typedef struct _cef_window_info_t {
   ///
+  /// Size of this structure.
+  ///
+  size_t size;
+
+  ///
   /// The initial title of the window, to be set when the window is created.
   /// Some layout managers (e.g., Compiz) can look at the window title
   /// in order to decide where to place the window when it is
@@ -174,6 +179,11 @@ typedef struct _cef_accelerated_paint_native_pixmap_plane_info_t {
 /// the callback returns from client code.
 ///
 typedef struct _cef_accelerated_paint_info_t {
+  ///
+  /// Size of this structure.
+  ///
+  size_t size;
+
   ///
   /// Planes of the shared texture, usually file descriptors of dmabufs.
   ///

commit f0d8200c4920d13640550cf2f38578ac2af2eb4b

Fix accelerated paint struct size init
Fixes: 81a5005bc0ec ("Add NUM_VALUES for enums and size for structs (see #3836)")

libcef/browser/osr/video_consumer_osr.cc
@@ -168,14 +168,16 @@ void CefVideoConsumerOSR::OnFrameCaptured(
 
 #if BUILDFLAG(IS_WIN)
     auto& gmb_handle = data->get_gpu_memory_buffer_handle();
-    cef_accelerated_paint_info_t paint_info;
+    cef_accelerated_paint_info_t paint_info = {
+        sizeof(cef_accelerated_paint_info_t)};
     paint_info.extra = extra;
     paint_info.shared_texture_handle = gmb_handle.dxgi_handle().buffer_handle();
     paint_info.format = pixel_format;
     view_->OnAcceleratedPaint(damage_rect, info->coded_size, paint_info);
 #elif BUILDFLAG(IS_APPLE)
     auto& gmb_handle = data->get_gpu_memory_buffer_handle();
-    cef_accelerated_paint_info_t paint_info;
+    cef_accelerated_paint_info_t paint_info = {
+        sizeof(cef_accelerated_paint_info_t)};
     paint_info.extra = extra;
     paint_info.shared_texture_io_surface = gmb_handle.io_surface().get();
     paint_info.format = pixel_format;
@@ -185,7 +187,8 @@ void CefVideoConsumerOSR::OnFrameCaptured(
     auto& native_pixmap = gmb_handle.native_pixmap_handle();
     CHECK(native_pixmap.planes.size() <= kAcceleratedPaintMaxPlanes);
 
-    cef_accelerated_paint_info_t paint_info;
+    cef_accelerated_paint_info_t paint_info = {
+        sizeof(cef_accelerated_paint_info_t)};
     paint_info.extra = extra;
     paint_info.plane_count = native_pixmap.planes.size();
     paint_info.modifier = native_pixmap.modifier;