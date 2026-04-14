3a0fcf1e1b6249b50c96ac77c429bfefade09d96
Marshall Greenblatt <magreenblatt@gmail.com>
2026-04-14 17:24:09 -0400

    Fix struct size compatibility checks (fixes #4153)

--- include/internal/cef_types_wrappers.h
+++ include/internal/cef_types_wrappers.h
@@ -470,7 +470,9 @@ struct CefSettingsTraits {
 #endif
 
 #if CEF_API_ADDED(14600)
-    target->use_views_default_popup = src->use_views_default_popup;
+    if (CEF_MEMBER_EXISTS(src, use_views_default_popup)) {
+      target->use_views_default_popup = src->use_views_default_popup;
+    }
 #endif
   }
 };
@@ -581,7 +583,9 @@ struct CefBrowserSettingsTraits {
     target->chrome_status_bubble = src->chrome_status_bubble;
     target->chrome_zoom_bubble = src->chrome_zoom_bubble;
 #if CEF_API_ADDED(CEF_EXPERIMENTAL)
-    target->ax_viewport_collapse = src->ax_viewport_collapse;
+    if (CEF_MEMBER_EXISTS(src, ax_viewport_collapse)) {
+      target->ax_viewport_collapse = src->ax_viewport_collapse;
+    }
 #endif
   }
 };
