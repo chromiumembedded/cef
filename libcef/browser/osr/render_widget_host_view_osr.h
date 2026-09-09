52d82d53266fac88a36cf5f865e131b417fa82ae
Sheen Tian <5869938+tishion@users.noreply.github.com>
2026-09-10 01:56:29 +0800

    mac: osr: Remove DidUpdateVisualProperties exclusion (fixes #4258) (#4259)
    This callback is necessary to feed a renderer child-allocated
    viz::LocalSurfaceId back into SynchronizeVisualProperties().

--- libcef/browser/osr/render_widget_host_view_osr.h
+++ libcef/browser/osr/render_widget_host_view_osr.h
@@ -183,10 +183,8 @@ class CefRenderWidgetHostViewOSR
   void TransformPointToRootSurface(gfx::PointF* point) override;
   gfx::Rect GetBoundsInScreen() override;
 
-#if !BUILDFLAG(IS_MAC)
   viz::ScopedSurfaceIdAllocator DidUpdateVisualProperties(
       const cc::RenderFrameMetadata& metadata) override;
-#endif
 
   viz::SurfaceId GetCurrentSurfaceId() const override;
   bool HasSavedCompositorFrame() const override;
